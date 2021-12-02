dc6e8204587fb95052d54bcb1a846f1e1efbfed7
Alvin Cousins <alvin.cousins@roh.org.uk>
2021-12-02 13:53:56 +0000

    day02 problem 2

--- src/day02.ts
+++ src/day02.ts
@@ -1,12 +1,16 @@
 import {posArr} from './day02Input';
 
-const pos: {horiz: number, depth: number} = {horiz: 0, depth: 0}
+const pos: {horiz: number, depth: number, aim: number} = {horiz: 0, depth: 0, aim: 0};
 
 posArr.forEach((cmdString)=> {
     const cmdArr= cmdString.split(" ")
-    if (cmdArr[0] === "forward") pos.horiz = pos.horiz + Number(cmdArr[1]);
-    if (cmdArr[0] === "up") pos.depth = pos.depth - Number(cmdArr[1]);
-    if (cmdArr[0] === "down") pos.depth = pos.depth + Number(cmdArr[1]);
+    if (cmdArr[0] === "up") pos.aim -= Number(cmdArr[1]);
+    if (cmdArr[0] === "down") pos.aim += Number(cmdArr[1]);
+
+    if (cmdArr[0] === "forward") {
+        pos.horiz += Number(cmdArr[1]);
+        pos.depth += pos.aim * Number(cmdArr[1]);
+    }
 })
 
 console.log("pos:", pos);
