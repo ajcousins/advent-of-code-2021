b0bf58efa68a87e447f3a70571054069d102c2a3
Alvin Cousins <alvin.cousins@roh.org.uk>
2021-12-12 17:42:41 +0000

    Day 12 part 1

--- src/day12/01.ts
+++ src/day12/01.ts
@@ -1,2 +1,37 @@
-const text = require('./input.txt');
+const text = require('./input_test.txt');
 export {}
+
+interface Graph {
+    adjacencyList: {
+        [key: string]: string[]
+    }
+}
+
+class Graph {
+    constructor() {
+        this.adjacencyList = {}
+    }
+    addVertex(vertex:string) {
+        if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+    }
+    addEdge(v1:string, v2:string) {
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
+    }
+}
+
+const inputList = text.default.split("\n")
+
+console.log(inputList);
+
+const caveGraph = new Graph();
+
+inputList.forEach((line:string) => {
+    const vertexes = line.split("-")
+    vertexes.forEach((vertex:string) => {
+        caveGraph.addVertex(vertex);
+    })
+    caveGraph.addEdge(vertexes[0], vertexes[1]);
+})
+
+console.log("caveGraph:", caveGraph);
