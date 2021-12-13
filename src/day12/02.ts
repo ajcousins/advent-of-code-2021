0aafcf720d8bcf66d7fc80f1d4987ce41777772a
Alvin Cousins <alvin.cousins@roh.org.uk>
2021-12-13 15:22:40 +0000

    Day 12 part 2

--- src/day12/02.ts
+++ src/day12/02.ts
@@ -21,14 +21,17 @@ class Graph {
     findPaths(start:string) {
         const allPossPaths:string[][] = [];
         const startingPath:string[] = [];
-        const startingVisited:{[key:string]:boolean} = {}
+        const startingVisited:{[key:string]:number} = {}
         const adjList = this.adjacencyList;
 
-        function helper(current:string, pathSoFar:string[], visited:any):null {
+        function helper(current:string, pathSoFar:string[], visited:any, doubleVis?:boolean):null {
             const pathCopy = [...pathSoFar, current];
             const visitedCopy = {...visited};
+            let doubleVisit = doubleVis ? doubleVis : false;
+
             if (current.charAt(0) === current.charAt(0).toLowerCase()) {
-                visitedCopy[current] = true;
+                if(visitedCopy[current]) doubleVisit = true;
+                visitedCopy[current] = 1;
             }
             // base case
             if (current === "end") {
@@ -39,11 +42,12 @@ class Graph {
             const toVisit:string[] = [];
             adjList[current].forEach((dest:string) => {
                 if (!visitedCopy[dest]) toVisit.push(dest)
+                else if (visitedCopy[dest] && !doubleVisit && dest !== start) toVisit.push(dest)
             })
             if (!toVisit.length) return null;
 
             toVisit.forEach((dest:string) => {
-                helper(dest, pathCopy, visitedCopy)
+                helper(dest, pathCopy, visitedCopy, doubleVisit)
             })
         }
         helper(start, startingPath, startingVisited)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'development',
-    entry: './src/day12/01.ts',
+    entry: './src/day12/02.ts',
     devtool: 'inline-source-map',
     module: {
         rules: [
