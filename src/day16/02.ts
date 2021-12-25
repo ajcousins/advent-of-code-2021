09ac9a1cd0c734f35595092377d150c81d3250cc
Alvin Cousins <alvin.cousins@roh.org.uk>
2021-12-25 00:32:55 +0000

    Day 16 part 2

--- src/day16/02.ts
+++ src/day16/02.ts
@@ -1,28 +1,20 @@
 const { hexToBin } = require('../lib/hexToBin');
 
-const text = require('./input_test.txt');
+const text = require('./input.txt');
 export {}
-
-
 
 interface Packet {
     version: number;
     typeID: number;
-    payload?: number | string;
+    payload?: number;
     children?: Packet[];
     binLength?: number;
 }
 
-
-
 const charArr = text.default.split("");
-
 const binInput = charArr.map((char:string) => hexToBin[char]).join("");
-console.log(binInput);
 
-// const output:Packet[] = [];
-
-function parseBin(bin:string, packetCount?:number):Packet {
+function parseBin(bin:string):Packet {
     if (bin.length < 11) return null;
     let input = [...bin.split("")]
     let version = parseInt(input.slice(0, 3).join(""), 2);
@@ -36,9 +28,7 @@ function parseBin(bin:string, packetCount?:number):Packet {
             bitArr.push(input.slice(1, 5).join(""))
             if (input[0] === "0") {
                 input.splice(0, 5);
-      
                 return {version, typeID, binLength: (bitArr.length * 5) + 6, payload: parseInt(bitArr.join(""), 2)}
-
             }
             input.splice(0, 5);
         }
@@ -47,41 +37,53 @@ function parseBin(bin:string, packetCount?:number):Packet {
     // Operator packet
     else {
         const lengthTypeID = input.shift();
+        const packetArr = []
+        let binLength;
+
         if (lengthTypeID === "0") {
             let subPackBinLength = parseInt(input.splice(0, 15).join(""), 2);
-            const packetArr = []
-
             while (subPackBinLength > 0) {
                 let result = parseBin(input.slice(0, subPackBinLength).join(""))
                 packetArr.push(result);
                 subPackBinLength -= result.binLength;
                 input.splice(0, result.binLength)
             }
-            const binLength = packetArr.reduce((a, b) => a + b.binLength, 0) + 22;
+            binLength = packetArr.reduce((a, b) => a + b.binLength, 0) + 22;
 
-            return {version, typeID, children: packetArr, binLength}
-
-        } 
-        else {
+        } else {
             let numberOfSubPackets = parseInt(input.splice(0, 11).join(""), 2);
-            const packetArr = []
-
             while (numberOfSubPackets > 0) {
                 let result = parseBin(input.join(""));
                 input.splice(0, result.binLength);
                 packetArr.push(result);
                 numberOfSubPackets--;
             }
-            const binLength = packetArr.reduce((a, b) => a + b.binLength, 0) + 18;
-
-            return {version, typeID, children: packetArr, binLength}
-
+            binLength = packetArr.reduce((a, b) => a + b.binLength, 0) + 18;
         }
+        return {version, typeID, children: packetArr, binLength, payload: evalChildren(packetArr, typeID)}
     }
 }
+
+function evalChildren(arr:Packet[], typeID:number):number {
+    switch(typeID) {
+        case 0:
+            return arr.reduce((p:number, c:Packet) => p + c.payload, 0)
+        case 1:
+            return arr.reduce((p:number, c:Packet) => p * c.payload, 1)
+        case 2:
+            return arr.reduce((p:number, c:Packet) => Math.min(p, c.payload), Infinity)
+        case 3:
+            return arr.reduce((p:number, c:Packet) => Math.max(p, c.payload), 0)
+        case 5:
+            return arr[0].payload > arr[1].payload ? 1 : 0
+        case 6:
+            return arr[0].payload < arr[1].payload ? 1 : 0
+        case 7:
+            return arr[0].payload === arr[1].payload ? 1 : 0
+        default:
+            return null
+    }
+}
+
 parseBin(binInput);
-
 console.log("output:", parseBin(binInput));
-
-// const versionSum = output.reduce((p:number, c:Packet) => c.version + p, 0)
-// console.log("versionSum:", versionSum);
